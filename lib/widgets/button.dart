52ecadaa0c0c9ef7fba932ec501ad6f51c8bef27
hideokawahara <kawahide.72@gmail.com>
2022-08-12 22:17:46 +0900

    feat: メッセージ遷移ボタン追加

--- lib/widgets/button.dart
+++ lib/widgets/button.dart
@@ -36,3 +36,42 @@ class LikeButton extends StatelessWidget {
     );
   }
 }
+
+class GoToMessageButton extends StatelessWidget {
+  const GoToMessageButton({
+    Key? key,
+    this.onTap,
+  }) : super(key: key);
+  final Function()? onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {
+        if (onTap != null) {
+          onTap!();
+        }
+      },
+      child: Container(
+        width: double.infinity,
+        padding: const EdgeInsets.symmetric(
+          horizontal: 10,
+          vertical: 12,
+        ),
+        decoration: BoxDecoration(
+          color: Colors.redAccent,
+          borderRadius: BorderRadius.circular(20),
+        ),
+        child: const Text(
+          'メッセージを送る',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 16,
+            fontWeight: FontWeight.w600,
+          ),
+          textAlign: TextAlign.center,
+        ),
+      ),
+    );
+  }
+}
