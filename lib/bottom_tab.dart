4e80ee7f6d23e73d6a4f05bacd28476d154083d8
hideokawahara <kawahide.72@gmail.com>
2022-08-12 13:20:00 +0900

    feat: ボトムタブ画面作成

---
+++ lib/bottom_tab.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class BottomTab extends StatelessWidget {
+  BottomTab({Key? key}) : super(key: key);
+
+  // タブに連動して表示させたい画面のリスト
+  final List<Widget> _pageWidgets = [
+    const Center(
+      child: Text("ホーム"),
+    ),
+    const Center(
+      child: Text("ユーザー"),
+    ),
+    const Center(
+      child: Text(
+        "お気に入り",
+      ),
+    ),
+    const Center(
+      child: Text(
+        "設定",
+      ),
+    ),
+  ];
+
+  // タブに実際に表示させたいアイコン等のリスト
+  final List<BottomNavigationBarItem> _tabItems = [
+    const BottomNavigationBarItem(
+      icon: Icon(Icons.home),
+      label: 'ホーム',
+    ),
+    const BottomNavigationBarItem(
+      icon: Icon(Icons.person),
+      label: 'ユーザー',
+    ),
+    const BottomNavigationBarItem(
+      icon: Icon(Icons.favorite),
+      label: 'お気に入り',
+    ),
+    const BottomNavigationBarItem(
+      icon: Icon(Icons.settings),
+      label: '設定',
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoTabScaffold(
+      tabBar: CupertinoTabBar(
+        items: _tabItems,
+        activeColor: Colors.yellowAccent,
+        inactiveColor: Colors.white,
+        backgroundColor: Colors.blueGrey,
+        // 追加したプロパティを記述する
+        height: 66,
+      ),
+      tabBuilder: (context, index) {
+        return CupertinoTabView(builder: (context) {
+          return _pageWidgets[index];
+        });
+      },
+    );
+  }
+}
