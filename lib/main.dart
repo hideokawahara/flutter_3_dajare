923d6c0a62aed2c1f211a5c7f9a0e342dcb685d8
hideokawahara <kawahide.72@gmail.com>
2022-08-27 15:34:49 +0900

    feat: ログイン時、ログアウト時の情報取得

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_3_dajare/bottom_tab.dart';
+import 'package:flutter_3_dajare/view_model/authentication_view_model.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,15 +11,40 @@ class MainApp extends StatelessWidget {
   const MainApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      initialRoute: '/home',
-      routes: {
-        '/home': (_) => BottomTab(),
-      },
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<AuthenticationViewModel>(
+          create: (_) => AuthenticationViewModel(),
+        ),
+      ],
+      child: Builder(builder: (newContext) {
+        return FutureBuilder<bool>(
+          future: Provider.of<AuthenticationViewModel>(newContext).login(),
+          builder: (_, snapshot) {
+            final String initialRoute;
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            } else if (snapshot.hasData) {
+              initialRoute = snapshot.data! ? '/home' : '/login';
+            } else {
+              initialRoute = '/login';
+            }
+            return MaterialApp(
+              title: 'Flutter Demo',
+              theme: ThemeData(
+                primarySwatch: Colors.blue,
+              ),
+              initialRoute: initialRoute,
+              routes: {
+                '/home': (_) => BottomTab(),
+                '/login': (_) => const Center(child: Text('ログインしてね')),
+              },
+            );
+          },
+        );
+      }),
     );
   }
 }
