17d1d6e5f4712fdf1239426b23ee6fd8150d0f88
hideokawahara <kawahide.72@gmail.com>
2022-08-31 22:47:01 +0900

    feat: 女性ホーム画面view model作成

--- lib/pages/female_home_page.dart
+++ lib/pages/female_home_page.dart
@@ -1,15 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_3_dajare/pages/man_details_page.dart';
+import 'package:flutter_3_dajare/view_model/female_home_view_model.dart';
 import 'package:flutter_3_dajare/widgets/button.dart';
+import 'package:provider/provider.dart';
 
 class FemaleAccountHomePage extends StatelessWidget {
   const FemaleAccountHomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Colors.white,
-      body: FemaleAccountHomePageBody(),
+    return ChangeNotifierProvider(
+      create: (_) => FemaleHomeViewModel(),
+      child: const Scaffold(
+        backgroundColor: Colors.white,
+        body: FemaleAccountHomePageBody(),
+      ),
     );
   }
 }
@@ -26,77 +31,85 @@ class _FemaleAccountHomePageBodyState extends State<FemaleAccountHomePageBody> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: ListView.builder(
-        padding: const EdgeInsets.symmetric(horizontal: 16),
-        itemCount: 6,
-        itemBuilder: (BuildContext listContext, int index) {
-          return Container(
-            // height: 100,
-            width: 200,
-            margin: const EdgeInsets.only(bottom: 16),
-            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(20.0),
-              border: Border.all(color: Colors.black12),
-              boxShadow: const [
-                BoxShadow(
-                  color: Colors.black26,
-                  spreadRadius: 1.0,
-                  blurRadius: 10.0,
-                  offset: Offset(10, 10),
-                ),
-              ],
+      child: Consumer<FemaleHomeViewModel>(
+        builder: (context, homeModel, child) {
+          return ListView.builder(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 16,
+              vertical: 24,
             ),
-            child: Column(
-              children: [
-                const Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text(
-                    "駄洒落を言ったのはダジャレ",
-                    style: TextStyle(
-                      fontSize: 14,
-                      fontWeight: FontWeight.bold,
-                      fontFamily: 'Shippori_Antique',
+            itemCount: homeModel.list.length,
+            itemBuilder: (BuildContext listContext, int index) {
+              return Container(
+                height: 320,
+                margin: const EdgeInsets.only(bottom: 16),
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(20.0),
+                  border: Border.all(color: Colors.black12),
+                  boxShadow: const [
+                    BoxShadow(
+                      color: Colors.black26,
+                      spreadRadius: 1.0,
+                      blurRadius: 10.0,
+                      offset: Offset(10, 10),
                     ),
-                  ),
+                  ],
                 ),
-                const Divider(),
-                Align(
-                  alignment: Alignment.centerRight,
-                  child: LikeButton(
-                    onTap: () async {
-                      print("tap");
-                      BuildContext innerContext =
-                          context; // 内部の context を保持しておくためのもの
-                      showDialog(
-                        context: context,
-                        barrierDismissible: false,
-                        builder: (BuildContext context) {
-                          innerContext =
-                              context; // builder で使われている context を innerContext に保存しておく
-                          return const Center(
-                            child: CircularProgressIndicator(),
+                child: Column(
+                  children: [
+                    Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        homeModel.list[index].contents,
+                        style: const TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          fontFamily: 'Shippori_Antique',
+                        ),
+                      ),
+                    ),
+                    const Spacer(),
+                    const Divider(),
+                    Align(
+                      alignment: Alignment.centerRight,
+                      child: LikeButton(
+                        onTap: () async {
+                          print("tap");
+                          BuildContext innerContext =
+                              context; // 内部の context を保持しておくためのもの
+                          showDialog(
+                            context: context,
+                            barrierDismissible: false,
+                            builder: (BuildContext context) {
+                              innerContext =
+                                  context; // builder で使われている context を innerContext に保存しておく
+                              return const Center(
+                                child: CircularProgressIndicator(),
+                              );
+                            },
+                          );
+                          await Future.delayed(
+                            const Duration(
+                              milliseconds: 1000,
+                            ),
+                          );
+                          if (!mounted) return;
+                          Navigator.of(innerContext).pop();
+                          Navigator.of(context, rootNavigator: true).push(
+                            MaterialPageRoute(
+                              builder: (_) => const ManDetailsPage(),
+                            ),
                           );
                         },
-                      );
-                      await Future.delayed(
-                        const Duration(
-                          milliseconds: 1000,
-                        ),
-                      );
-                      if (!mounted) return;
-                      Navigator.of(innerContext).pop();
-                      Navigator.of(context, rootNavigator: true).push(
-                        MaterialPageRoute(
-                          builder: (_) => const ManDetailsPage(),
-                        ),
-                      );
-                    },
-                  ),
+                      ),
+                    ),
+                  ],
                 ),
-              ],
-            ),
+              );
+            },
           );
         },
       ),
---
+++ lib/view_model/female_home_view_model.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+import '../model/joke.dart';
+
+class FemaleHomeViewModel extends ChangeNotifier {
+  //Todo: 修正する
+  final List<Joke> _list = _dummyJokeList;
+  List<Joke> get list {
+    return [..._list];
+  }
+}
+
+List<Joke> _dummyJokeList = [
+  Joke(
+    authorName: 'ユーキ',
+    authorAge: 28,
+    authorImage:
+        'https://pakutaso.cdn.rabify.me/shared/img/thumb/danda-danda-danda-_MKT5249-11344-11395-11417.jpg.webp?d=1420',
+    contents: 'Q：小野洋子さんはどこにいますか？\n「オノヨーコ、そおのよーこ」',
+    likesCounts: 2748,
+  ),
+  Joke(
+    authorName: 'こーへい',
+    authorAge: 32,
+    authorImage:
+        'https://pakutaso.cdn.rabify.me/shared/img/thumb/KENTA883_hansyaban.jpg.webp?d=1420',
+    contents: '水原希子に自ら聞こう。',
+    likesCounts: 1808,
+  ),
+  Joke(
+    authorName: '三上',
+    authorAge: 29,
+    authorImage:
+        'https://pakutaso.cdn.rabify.me/shared/img/thumb/MAX20160I9A9490.jpg.webp?d=1420',
+    contents: '肉まんを憎まん',
+    likesCounts: 1511,
+  ),
+  Joke(
+    authorName: 'ゆうくん',
+    authorAge: 22,
+    authorImage:
+        'https://pakutaso.cdn.rabify.me/shared/img/thumb/ookawa_MKT4826-11759.jpg.webp?d=1420',
+    contents: '小野小町、斧のコマネチ',
+    likesCounts: 1027,
+  ),
+];
