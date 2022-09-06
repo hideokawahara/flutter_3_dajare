311dd4f48b4e4639e9b0ea85389e3470e658799b
hideokawahara <kawahide.72@gmail.com>
2022-09-06 23:47:08 +0900

    feat: 自己紹介編集画面作成

---
+++ lib/pages/message_edit_page.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_3_dajare/widgets/icon.dart';
+
+class MessageEditPage extends StatelessWidget {
+  const MessageEditPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: const Text(
+          '自己紹介',
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        leading: InkWell(
+          onTap: () {
+            Navigator.of(context).pop();
+          },
+          child: const BackArrowIcon(),
+        ),
+      ),
+      body: const MessageEditPageBody(),
+    );
+  }
+}
+
+class MessageEditPageBody extends StatelessWidget {
+  const MessageEditPageBody({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
